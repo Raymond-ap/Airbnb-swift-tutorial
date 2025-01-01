051e440e27e86f3c5afc1cded869d9420cd6f0f9
Raymond Apungu <apunguray@gmail.com>
2025-01-01 23:28:53 +0000

    chore: added custom picker view

---
+++ Airbnb/Core/Components/CollapsedPickerView.swift
@@ -0,0 +1,24 @@
+struct CollapsedPickerView: View {
+    var title: String
+    var description: String
+    
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Text(title)
+                    .foregroundStyle(.gray)
+                
+                Spacer()
+                
+                Text(description)
+            }
+            .fontWeight(.semibold)
+        }
+        .padding()
+        .background(.white)
+        .clipShape(RoundedRectangle(cornerRadius: 12))
+        .padding()
+        .shadow(radius: 10)
+    }
+}
---
+++ Airbnb/Core/Explore/DestinationSearchView.swift
@@ -0,0 +1,18 @@
+//
+//  DestinationSearchView.swift
+//  Airbnb
+//
+//  Created by Mac on 01/01/2025.
+//
+
+import SwiftUI
+
+struct DestinationSearchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DestinationSearchView()
+}
